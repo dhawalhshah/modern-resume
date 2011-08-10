ce836b7fe9410324f3ff3ba23b185ce21c1d1c17
Dhawal Shah <dhawalhshah@gmail.com>
2011-08-09 20:24:09 -0500

    Added angular

---
+++ js/controller.js
@@ -0,0 +1,36 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function ResumeJobsCtrl(){
+    this.jobs =[
+    {
+        "company": "Facebook",
+        "date":"2005-2007",
+        "title":"Senior Interface Designer",
+        "desc":"Intrinsicly enable optimal core competencies through corporate relationships. \n\
+                Phosfluorescently implement worldwide vortals and client-focused imperatives. Conveniently initiate virtual paradigms and top-line convergence."
+    },
+    {
+        "company": "Apple Inc",
+        "date":"2005-2007",
+        "title":"Senior Interface Designer",
+        "desc":"Progressively reconceptualize multifunctional outside the box thinking through inexpensive methods of empowerment. Compellingly morph extensive niche markets with mission-critical ideas. Phosfluorescently deliver bricks-and-clicks strategic theme areas rather than scalable benefits."
+    },
+    {
+        "company": "Microsoft",
+        "date":"2004-2005",
+        "title":"Principal and Creative Lead",
+        "desc":"Intrinsicly transform flexible manufactured products without excellent intellectual capital. Energistically evisculate orthogonal architectures through covalent action items. Assertively incentivize sticky platforms without synergistic materials."
+    },
+    {
+        "company": "International Business Machines (IBM)",
+        "date":"2001-2004",
+        "title":"Lead Web Designer",
+        "desc":"Globally re-engineer cross-media schemas through viral methods of empowerment. Proactively grow long-term high-impact human capital and highly efficient innovation. Intrinsicly iterate excellent e-tailers with timely e-markets."
+    }
+    ];
+    
+    
+}
+
