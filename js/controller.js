bf0d503fde6ca2bc43e14549dfd3d105d10c2abc
Dhawal Shah <dhawal@mentat-air.gateway.2wire.net>
2011-08-14 17:18:57 -0500

    Converted more of the site to angular

--- js/controller.js
+++ js/controller.js
@@ -3,6 +3,23 @@
  * and open the template in the editor.
  */
 function ResumeJobsCtrl(){
+    this.profile = "Progressively evolve cross-platform ideas before impactful infomediaries. Energistically visualize tactical initiatives before cross-media catalysts for change.";
+
+    this.topSkills = [
+    {
+        "skill":"Web Design",
+        "desc":"Assertively exploit wireless initiatives rather than synergistic core competencies."
+    },
+    {
+        "skill":"Interface Design",
+        "desc":"Credibly streamline mission-critical value with multifunctional functionalities"
+    },
+    {
+        "skill":"Project Direction",
+        "desc":"Proven ability to lead and manage a wide variety of design and development projects in team and independent situations."
+    }
+    ];
+
     this.jobs =[
     {
         "company": "Facebook",
@@ -28,6 +45,15 @@ function ResumeJobsCtrl(){
         "date":"2001-2004",
         "title":"Lead Web Designer",
         "desc":"Globally re-engineer cross-media schemas through viral methods of empowerment. Proactively grow long-term high-impact human capital and highly efficient innovation. Intrinsicly iterate excellent e-tailers with timely e-markets."
+    }
+    ];
+
+    this.education = [
+    {
+            "university":"Indiana University - Bloomington, Indiana",
+            "date":"2001-2004",
+            "degree":"Dual Major, Economics and English",
+            "gpa" : "4.0"
     }
     ];
     
