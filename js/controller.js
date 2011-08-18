e5756f4ec6fbb73d3f1a7f9aff074b965232da74
Dhawal Shah <dhawalhshah@gmail.com>
2011-08-18 01:32:52 -0500

    Added a filter to format date from start and end date mentioned in the model

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,3 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 function ResumeCtrl(){
     
     this.resume = {
@@ -27,26 +23,30 @@ function ResumeCtrl(){
             "data":[
             {
                 "company": "Facebook",
-                "date":"2005-2007",
+                "startDate":"2005-03-01",
+                "endDate":"2007-08-01",
                 "title":"Senior Interface Designer",
                 "desc":"Intrinsicly enable optimal core competencies through corporate relationships. \n\
                 Phosfluorescently implement worldwide vortals and client-focused imperatives. Conveniently initiate virtual paradigms and top-line convergence."
             },
             {
                 "company": "Apple Inc",
-                "date":"2005-2007",
+                "startDate":"2005-03-01",
+                "endDate":"2007-08-01",
                 "title":"Senior Interface Designer",
                 "desc":"Progressively reconceptualize multifunctional outside the box thinking through inexpensive methods of empowerment. Compellingly morph extensive niche markets with mission-critical ideas. Phosfluorescently deliver bricks-and-clicks strategic theme areas rather than scalable benefits."
             },
             {
                 "company": "Microsoft",
-                "date":"2004-2005",
+                "startDate":"2004-01-01",
+                "endDate":"2005-06-01",
                 "title":"Principal and Creative Lead",
                 "desc":"Intrinsicly transform flexible manufactured products without excellent intellectual capital. Energistically evisculate orthogonal architectures through covalent action items. Assertively incentivize sticky platforms without synergistic materials."
             },
             {
                 "company": "International Business Machines (IBM)",
-                "date":"2001-2004",
+                "startDate":"2001-03-01",
+                "endDate":"2005-07-01",
                 "title":"Lead Web Designer",
                 "desc":"Globally re-engineer cross-media schemas through viral methods of empowerment. Proactively grow long-term high-impact human capital and highly efficient innovation. Intrinsicly iterate excellent e-tailers with timely e-markets."
             }
@@ -59,7 +59,8 @@ function ResumeCtrl(){
             "data":[
             {
                 "university":"Indiana University - Bloomington, Indiana",
-                "date":"2001-2004",
+                "startDate":"2001-03-01",
+                "endDate":"2005-08-01",
                 "degree":"Dual Major, Economics and English",
                 "gpa" : "4.0"
             }
@@ -118,4 +119,20 @@ function ResumeCtrl(){
     
     
 }
+
+var month=new Array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec");
+
+angular.filter('period',function(startDate,endDate){
+    startDateObj = new Date(startDate);
+    out = month[startDateObj.getMonth()] + ', ' + startDateObj.getFullYear() + ' - ';
+    if(endDate){
+        endDateObj = new Date(endDate);
+        out = out + month[endDateObj.getMonth()] + ', ' + endDateObj.getFullYear();
+    }
+    else{
+        out = out + 'current';
+    }
+
+    return out;
+});
 
