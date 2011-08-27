1a1bab6b7ee96cd1d64b4dd0eb606bbeeea4d46d
Dhawal Shah <dhawalhshah@gmail.com>
2011-08-27 01:51:12 -0500

    Seperated from model from controller

--- js/controller.js
+++ js/controller.js
@@ -1,121 +1,13 @@
-function ResumeCtrl(){
+function ResumeCtrl($xhr){
     
-    this.resume = {
-        "order":['profile','topSkills','education','job'],
-        "about":{
-            "name":"Jonathan Doe",
-            "title":"Web Designer, Director",
-            "email":"name@yourdomain.com",
-            "phone":"(313) - 867-5309",        
-            "social":[
-            {
-                "icon":"google-plus.png",
-                "url":"http://www.google.com",
-                "title":"Google+"
-            },
-            {
-                "icon":"facebook.png",
-                "url":"http://www.facebook.com",
-                "title":"Facebook"
-            },
-            {
-                "icon":"github.png",
-                "url":"http://www.githunb.com",
-                "title":"Github"
-            }
-            ]
-        },
-        "sections":
-        [
-        {
-            "title":"Profile",
-            "type":"profile",
-            "name":'profile',  
-            "data":'"Progressively evolve cross-platform ideas before impactful infomediaries. Energistically visualize tactical initiatives before cross-media catalysts for change.'
-        },
-        {
-            "title":"Experience",
-            "type":"job",
-            "name":'job',
-            "data":[
-            {
-                "company": "Facebook",
-                "startDate":"2005-03-01",
-                "endDate":"2007-08-01",
-                "url":"http://www.facebook.com",
-                "title":"Senior Interface Designer",
-                "desc":"Intrinsicly enable optimal core competencies through corporate relationships. \n\
-                Phosfluorescently implement worldwide vortals and client-focused imperatives. Conveniently initiate virtual paradigms and top-line convergence."
-            },
-            {
-                "company": "Apple Inc",
-                "startDate":"2005-03-01",
-                "endDate":"2007-08-01",
-                "url":"http://www.apple.com",              
-                "title":"Senior Interface Designer",
-                "desc":"Progressively reconceptualize multifunctional outside the box thinking through inexpensive methods of empowerment. Compellingly morph extensive niche markets with mission-critical ideas. Phosfluorescently deliver bricks-and-clicks strategic theme areas rather than scalable benefits."
-            },
-            {
-                "company": "Microsoft",
-                "startDate":"2004-01-01",
-                "endDate":"2005-06-01",
-                "url":"http://www.microsoft.com", 
-                "title":"Principal and Creative Lead",
-                "desc":"Intrinsicly transform flexible manufactured products without excellent intellectual capital. Energistically evisculate orthogonal architectures through covalent action items. Assertively incentivize sticky platforms without synergistic materials."
-            },
-            {
-                "company": "International Business Machines (IBM)",
-                "startDate":"2001-03-01",
-                "endDate":"2005-07-01",
-                "url":"http://www.ibm.com",
-                "title":"Lead Web Designer",
-                "desc":"Globally re-engineer cross-media schemas through viral methods of empowerment. Proactively grow long-term high-impact human capital and highly efficient innovation. Intrinsicly iterate excellent e-tailers with timely e-markets."
-            }
-            ]
-        },
-        {
-            "title":"Education",
-            "type":"edu",
-            "name":'education',
-            "data":[
-            {
-                "university":"Indiana University - Bloomington, Indiana",
-                "startDate":"2001-03-01",
-                "endDate":"2005-08-01",
-                "degree":"Dual Major, Economics and English",
-                "gpa" : "4.0"
-            }
-            ]
-        },
-        {
-            "title":"Skills",
-            "type":"topSkills",
-            "name":'topSkills',
-            "data":[
-            {
-                "skill":"Web Design",
-                "desc":"Assertively exploit wireless initiatives rather than synergistic core competencies."
-            },
-            {
-                "skill":"Interface Design",
-                "desc":"Credibly streamline mission-critical value with multifunctional functionalities"
-            },
-            {
-                "skill":"Project Direction",
-                "desc":"Proven ability to lead and manage a wide variety of design and development projects in team and independent situations."
-            }
-            ]
-        },
-        {
-            "title":"Technical",
-            "type":"skills",
-            "name":"skills",
-            "data":[ "XHTML","CSS", "Javascript", "Jquery","PHP","CVS/Subversion","OS X","Windows XP/Vista","Linux"]
-        }
-        ]
-    
-    };
-       
+    var self = this;
+ 
+    $xhr('GET', 'js/resume.json', function(code, response) {
+        self.resume = response;        
+        console.log(self.resume); 
+    });
+
+   
     this.assets = {
         'external' : 'images/external.png'
     }; 
@@ -219,7 +111,6 @@ angular.filter('external',function(url){
     a.setAttribute("target","_blank");         
     a.setAttribute("class", "external")
     a.appendChild(img);
-    
     this.$element.append( a );
 });
 
